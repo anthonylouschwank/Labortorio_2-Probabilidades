2e382907752e5e6acd5bd1cac65eeefe89f7b6c0
Anthony Lou Schwank <123795140+anthonylouschwank@users.noreply.github.com>
2025-02-03 17:06:27 -0600

    Update Ejercicio_0.py
    Inciso C de ejercicio 0 terminado

--- Ejercicio_0.py
+++ Ejercicio_0.py
@@ -15,3 +15,15 @@ muestras_R = len(df[df['Red'] >= 10])
 P_R = muestras_R / total_muestras
 
 print(f"Probabilidad P(R) de tener al menos 10 botonetas rojas: {P_R:.4f}")
+
+# c) Calcular P(W): probabilidad de pesar al menos 50 gramos
+
+# Contar cuántos valores cumplen la condición
+cantidad_cumple = (bol["Weight"] >= 50).sum()
+
+# Calcular la probabilidad en porcentaje
+probabilidad = cantidad_cumple / 30
+resultado = probabilidad * 100
+
+# Imprimir el resultado correctamente usando f-string
+print(f"La probabilidad de elegir aleatoriamente y que la bolsa pese más de 50 es {resultado:.2f}%")
